a9c3b55132cf6a2335ab44e750d70ff75929e667
Stefan Backor <stevo@backor.sk>
2025-11-24 15:35:17 +0100

    Add manifest

--- apps/botc/src/main.tsx
+++ apps/botc/src/main.tsx
@@ -17,7 +17,7 @@ if (!rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <Theme
-        accentColor="red"
+        accentColor="purple"
         appearance="light"
         panelBackground="solid"
         hasBackground={false}
